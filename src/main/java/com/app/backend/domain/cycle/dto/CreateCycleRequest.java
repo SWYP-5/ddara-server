edb88b19fa36156ac7af903eb710c883c2d5f521
Jiwon Oh <ohjiwon3040@gmail.com>
2026-07-01 13:58:55 +0900

    feat: 모임명 및 회차 주제 최대 20자로 제한 #34

--- src/main/java/com/app/backend/domain/cycle/dto/CreateCycleRequest.java
+++ src/main/java/com/app/backend/domain/cycle/dto/CreateCycleRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateCycleRequest(
-        @NotBlank @Size(max = 100) String topic,
+        @NotBlank @Size(max = 20) String topic,
         @NotBlank String imageUrl
 ) {
 }
